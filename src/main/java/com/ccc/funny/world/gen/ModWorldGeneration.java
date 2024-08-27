0dcff6c261c9ffbd5a0442bd872f776dc3ffb098
Crazy-Cream-Cheese <lyqhadaba@outlook.com>
2024-08-27 14:45:02 +0800

    Delete src/main directory

--- src/main/java/com/ccc/funny/world/gen/ModWorldGeneration.java
+++
@@ -1,8 +0,0 @@
-package com.ccc.funny.world.gen;
-
-public class ModWorldGeneration {
-    public static void generateModWorldGen(){
-        ModOreGeneration.generateOres();
-        ModTreeGeneration.registerTrees();
-    }
-}
